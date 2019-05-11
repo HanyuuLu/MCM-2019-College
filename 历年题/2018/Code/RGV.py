3309e4ca027c46f66c88d47b4aabb87bbeefb52e
Hanyuu Furude <HanyuuFurude@outlook.com>
2019-05-11 13:23:55 +0800

    201905111323

---
+++ 历年题/2018/Code/RGV.py
@@ -0,0 +1,14 @@
+class RGV:
+	def __init__(self):
+		self.distenceTable = \
+			[
+				[20,33,46],
+				[23,41,59],
+				[18,32,46]
+			]
+		self.washTimeTable = \
+			[
+				25,
+				30,
+				25
+			]
