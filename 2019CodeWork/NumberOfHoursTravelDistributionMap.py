2b690e615da814ef834e8f437a194b31c3963e4b
Hanyuu <HanyuuFurude@outlook.com>
2019-05-17 01:09:42 +0800

    201905170109

--- 2019CodeWork/NumberOfHoursTravelDistributionMap.py
+++ 2019CodeWork/NumberOfHoursTravelDistributionMap.py
@@ -2,14 +2,14 @@ import os
 def drawGraphs():
     pass
 def numberOfHoursDistributionMap(res: dict) -> list:
-    res = list()
+    resList = list()
     for i in range(25):
-        res.append(0)
-    for key, value in os.environ.items(res):
-        for i in value.record:
+        resList.append(0)
+    for value in res:
+        for i in res[value].record:
             try:
-                res[i[0].hour] += 1
+                resList[i[0].hour] += 1
             except Exception as e:
-                res[24] += 1
-    print(res)
+                resList[24] += 1
+    return resList
 
