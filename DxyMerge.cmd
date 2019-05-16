10afbad28af7fcc50fdbcebf0a4d625b373978c8
Hanyuu <HanyuuFurude@outlook.com>
2019-05-16 21:29:54 +0800

    201905162129

---
+++ DxyMerge.cmd
@@ -0,0 +1,16 @@
+git checkout master
+git pull
+git merge dev-Dxy
+git push
+git checkout dev-Dxy
+git pull origin master
+git push
+git checkout dev-Yzh
+git pull
+git pull origin master
+git push
+git checkout dev-Hanyuu
+git pull
+git pull origin master
+git push
+git checkout dev-Dxy
---
+++ HanyuuMerge.cmd
@@ -0,0 +1,16 @@
+git checkout master
+git pull
+git merge dev-Hanyuu
+git push
+git checkout dev-Hanyuu
+git pull origin master
+git push
+git checkout dev-Yzh
+git pull
+git pull origin master
+git push
+git checkout dev-Dxy
+git pull
+git pull origin master
+git push
+git checkout dev-Hanyuu
