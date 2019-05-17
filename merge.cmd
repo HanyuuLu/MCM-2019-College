ab135eea8ec4b39b6b3ce393a031335207dd282f
Hanyuu <HanyuuFurude@outlook.com>
2019-05-18 00:42:33 +0800

    201905180042

--- merge.cmd
+++ merge.cmd
@@ -2,9 +2,14 @@ git pull
 git push
 git checkout master
 git merge dev-Yzh
+git push
+git pull
 git merge dev-Dxy
+git push
+git pull
 git merge dev-Hanyuu
 git push
+git pull
 git checkout dev-Yzh
 git pull
 git pull origin master
