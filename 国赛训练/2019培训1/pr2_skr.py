378bf38111f77f6cb541b4f61e5888b995052d88
Hanyuu Furude <HanyuuFurude@outlook.com>
2019-06-30 09:44:01 +0800

    201906300943

--- 国赛训练/2019培训1/pr2_skr.py
+++ 国赛训练/2019培训1/pr2_skr.py
@@ -1,15 +1,40 @@
-import csv
-
+import xlrd
 from sklearn import linear_model
-clf = linear_model.LinearRegression()
-# x = [[t.x1,t.x2,t.x3,t.x4,t.x5] for t in self.trainingTexts]
-# y = [t.human_rating for t in self.trainingTexts]
-x=[[1,2,2],[2,2,2],[3,4,4],[4,4,4],[5,5,1]]
-y = [1,2,3,4,5]
-aa = clf.fit(x,y)
+import sys
 
-regress_coefs = clf.coef_
-regress_intercept = clf.intercept_   
-print(regress_coefs)
-print(regress_intercept) 
-pass
+
+
+if __name__ == "__main__":
+	try:
+		fileName = sys.argv[-1]
+	except Exception:
+		print('[error] Please input file name')
+		exit()
+	x = list()
+	y = list()
+	try:
+		workbook = xlrd.open_workbook(fileName)
+		sheet = workbook.sheet_by_index(0)
+		for i in range(sheet.nrows):
+			tempX = list()
+			for j in range(sheet.ncols-1):
+				tempX.append(sheet.cell(i,j).value)
+			x.append(tempX)
+			y.append(sheet.cell(i,sheet.ncols-1).value)
+	except Exception:
+		print('[error] There is problem with input file name %s'%fileName)
+		exit()
+	print('[info] file %s loaded successfully, fitting for the model'%fileName)
+	clf = linear_model.LinearRegression()
+	# print(x)
+	# print(y)
+	clf.fit(x,y)
+
+	regress_coefs = clf.coef_
+	regress_intercept = clf.intercept_
+	print('[param]\t',clf.get_params())
+	for i in range(len(regress_coefs)):
+		print('[INT %d]\t%f'%(i,regress_coefs[i]))
+	print('[coef]\t%f'%regress_intercept) 
+	print('[score]\t',clf.score(x,y))
+
